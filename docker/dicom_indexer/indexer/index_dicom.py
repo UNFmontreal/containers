f18e914311e3e1c67981da81c724a2f17c80bf5a
Milton Camacho <milton.camachocamach@ucalgary.ca>
2024-01-25 19:17:40 +0000

    wip: dicom indexer

--- docker/dicom_indexer/indexer/index_dicom.py
+++ docker/dicom_indexer/indexer/index_dicom.py
@@ -301,7 +301,7 @@ def import_local_data(
         dest = dest + ".7z"
         # create 7z archive with 1block/file parameters
         subprocess.run(
-            ["7z", "u", str(dest), "."] + p7z_opts,
+            ["7z", "u", str(dest), "."].append(p7z_opts),
             cwd=dicom_session_ds.path,
         )
     elif input_path.is_file():

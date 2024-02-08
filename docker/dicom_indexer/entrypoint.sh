fe95059f1c990575683b618a272d2e799f1df1f7
bpinsard <basile.pinsard@gmail.com>
2024-02-08 13:40:01 -0500

    wip

--- docker/dicom_indexer/entrypoint.sh
+++ docker/dicom_indexer/entrypoint.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 CONTAINER_ID=$(basename $(cat /proc/1/cpuset))
-GITLAB_TOKEN_SECRET=/var/run/secrets/dicom_bot_gitlab_token
+GITLAB_TOKEN_SECRET=$(cat /var/run/secrets/dicom_bot_gitlab_token 2>/dev/null)
+GITLAB_TOKEN=${GITLAB_TOKEN_SECRET:=$GITLAB_TOKEN}
 
 # only export keys when deploying as a service on swarm
 # TODO: should try using gitlab runner mechanism if not
-if [ -e $GITLAB_TOKEN_SECRET ] ; then
+if [ -n "${GITLAB_TOKEN}" ] ; then
 	# generate container specific ssh-key
 	ssh-keygen -f  /root/.ssh/id_rsa -N ''
 	# register it for dicom_bot user
-	curl -X POST -F "private_token=$(cat $GITLAB_TOKEN_SECRET)" \
+	echo 'registering the ssh key'
+	ssh_key_json=$(curl -X POST -F "private_token=${GITLAB_TOKEN}" \
 	  -F "title="$(cat /etc/hostname)${CONTAINER_ID:0:12} -F "key=$(cat ~/.ssh/id_rsa.pub)" \
-	  "${GITLAB_API_URL}/user/keys"
+	  "${GITLAB_API_URL}/user/keys")
 	fi
 
 git config --global init.defaultBranch main
-
+mkdir -p ~/.ssh/known_hosts
+install -m 600 /dev/stdin ~/.ssh/known_hosts <<< "$SSH_KNOWN_HOSTS"
 
 # example
 # /usr/bin/storescp \
@@ -26,12 +29,12 @@ git config --global init.defaultBranch main
 #  --exec-on-eostudy "python3 $DICOM_ROOT/exec_on_study_received.py #p " 2100 >> $DICOM_DATA_ROOT/storescp.log
 
 # run whatever command was passed (storescp or python index_dicoms directly)
-echo $@
-bash -c "$@"
+$@
 
-if [ -e $GITLAB_TOKEN_SECRET ] ; then
+if [ -n "${GITLAB_TOKEN}" ] ; then
 	# unregister the temporary ssh key
-	curl -X DELETE -F "private_token=$(cat $GITLAB_TOKEN_SECRET)" \
+	ssh_key_id=$(jq .id <<< $ssh_key_json)
+	curl -X DELETE -F "private_token=${GITLAB_TOKEN}" \
 	  -F "title="$(cat /etc/hostname)${CONTAINER_ID:0:12}
-	  "${GITLAB_API_URL}/user/keys"
+	  "${GITLAB_API_URL}/users/keys/${ssh_key_id}"
 fi
